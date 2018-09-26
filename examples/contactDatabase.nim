851d8f5f9a5d5758344d1a862cfa6bbd53529920
David Krause <david@code0.xyz>
2018-09-26 15:29:06 +0200

    improve contac example
    Signed-off-by: David Krause <david@code0.xyz>

--- examples/contactDatabase.nim
+++ examples/contactDatabase.nim
@@ -1,16 +1,15 @@
-import ../flatdb
+import ../src/flatdb
 import json
 import tables
-
+import os
 # A simple contacts database example
 
 
 var contacts = newFlatDb("contacts.db", false)
 discard contacts.load() # we load all the data from file
 
-if contacts.nodes.len == 0:
-  # if no data there we create some testdata.
-  discard contacts.append(%* {"name": "Klaus", "phone": "012 8378 89", "country": "Germany", "email": "Klaus@example.org"})
+if contacts.nodes.len == 0: # if no data there we create some testdata.
+  discard contacts.append(%* {"name": "Klaus", "phone": "012 8378 89", "country": "Germany", "email": "Klaus@example.org", "someUniqueKey": "foo"})
   discard contacts.append(%* {"name": "Max Musterman", "phone": "787 78 98", "country": "Germany", "email": "Max.Musterman@example.org"})
   discard contacts.append(%* {"name": "Peter", "phone": "388983 89 89", "country": "Austria", "email": "Peter@example.org"})
   discard contacts.append(%* {"name": "Hans", "phone": "889 8 98 89", "country": "Netherlands", "email": "Hans@example.org"})
@@ -20,6 +19,9 @@ if contacts.nodes.len == 0:
   discard contacts.append(%* {"name": "John Doe", "phone": "6123 1376 837812 39", "country": "Usa", "email": "John.Doe@example.org"})
 
 
+echo contacts.query equal("name", "Klaus")
+echo contacts.query has("someUniqueKey") or equal("country", "Netherlands")
+echo contacts.query equal("name", "Иван Петрович Сидоров")
 
 
-    
+removeFile("./contacts.db") # to cleanup after this example
